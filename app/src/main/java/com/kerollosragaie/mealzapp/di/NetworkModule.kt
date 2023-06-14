40b87f063d32eee4feb4ac562b13563c595e1ede
kerolosragaie <kerolosragaie@gmail.com>
2023-06-14 21:22:02 +0200

    Creating di for NetworkModule

--- app/src/main/java/com/kerollosragaie/mealzapp/di/NetworkModule.kt
+++ app/src/main/java/com/kerollosragaie/mealzapp/di/NetworkModule.kt
@@ -1,5 +1,36 @@
 package com.kerollosragaie.mealzapp.di
 
+import com.kerollosragaie.data.remote.ApiServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideOkHttp(): OkHttpClient =
+        OkHttpClient.Builder().connectTimeout(20, TimeUnit.SECONDS).build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .baseUrl("www.themealdb.com/api/json/v1/1/")
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideApiServices(retrofit: Retrofit): ApiServices =
+        retrofit.create(ApiServices::class.java)
+
 }
